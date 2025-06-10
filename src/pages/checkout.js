6015c7f48462292eec7a19c30de41bd922a0e6f8
anyasauce <anyajb014@gmail.com>
2025-06-10 16:16:24 +0800

    update

--- src/pages/checkout.js
+++ src/pages/checkout.js
@@ -37,7 +37,7 @@ export default function Checkout() {
                     email: form.email,
                     amount: amountInCentavos,
                     secretKey: form.secretKey || undefined,
-                    redirect_url: `${process.env.NEXT_PUBLIC_APP_URL}/payment-status`,
+                    returnUrl: 'https://paymongo-api.vercel.app/payment-status',
                 }),
             });
 
